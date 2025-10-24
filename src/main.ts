c29156e588fe5ec211cba462d831ab6980592212
Gonzalo Agustín Nozikovsky <103380329+ganozikovsky@users.noreply.github.com>
2025-10-24 09:36:41 -0300

    fix: port

--- src/main.ts
+++ src/main.ts
@@ -24,8 +24,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  const port = process.env.PORT || 8080;
-  await app.listen(port);
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+  await app.listen(port, '0.0.0.0');
   console.log(`Server is running on port ${port}`);
 }
 bootstrap();
